80a11334909568716cb11598376dd8562c0e9e79
Nick Veys <nick@codelever.com>
2018-03-22 15:30:41 -0500

    Add basic cmake task

---
+++ lib/mix/tasks/compile/cmake.ex
@@ -0,0 +1,13 @@
+defmodule Mix.Tasks.Compile.Cmake do
+  @moduledoc "Builds native source using CMake"
+  use Mix.Task
+
+  def run(_) do
+    :ok = File.mkdir_p("_cmake")
+    {result, _} = System.cmd("cmake", [".."], cd: "_cmake", stderr_to_stdout: true)
+    Mix.shell.info result
+    {result, _} = System.cmd("make", ["all"], cd: "_cmake", stderr_to_stdout: true)
+    Mix.shell.info result
+    :ok
+  end
+end
