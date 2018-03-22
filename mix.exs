88f7069aecaada6d2ed76a9d5daf0e5bd7db8f53
Nick Veys <nick@codelever.com>
2018-03-22 15:31:19 -0500

    Add some basic details to mix.exs

--- mix.exs
+++ mix.exs
@@ -5,8 +5,10 @@ defmodule ElixirCmake.MixProject do
     [
       app: :elixir_cmake,
       version: "0.1.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
+      source_url: "https://github.com/code-lever/elixir-cmake",
+      homepage_url: "https://github.com/code-lever/elixir-cmake",
       deps: deps()
     ]
   end
@@ -21,8 +23,6 @@ defmodule ElixirCmake.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
   end
 end
