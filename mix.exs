aa319ef22e59fa7a44fe249865eaf26c6dd9ffac
Nick Veys <nick@codelever.com>
2018-03-22 15:34:43 -0500

    More mixfile cleanup

--- mix.exs
+++ mix.exs
@@ -5,22 +5,20 @@ defmodule ElixirCmake.MixProject do
     [
       app: :elixir_cmake,
       version: "0.1.0",
-      elixir: "~> 1.5",
-      start_permanent: Mix.env() == :prod,
+      elixir: "~> 1.3",
+      build_embedded: Mix.env() == :prod,
       source_url: "https://github.com/code-lever/elixir-cmake",
       homepage_url: "https://github.com/code-lever/elixir-cmake",
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
     ]
